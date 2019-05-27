f33316dc13ea2294476b3c4d72c3f914806df522
Johnny <johnny@offspringdigital.com>
2019-05-27 14:26:05 +0700

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskDetailComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
